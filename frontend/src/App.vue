87de4d1bf99a47e74578571db2efdf11baea406a
patrickdokter <patdokter@gmail.com>
2017-12-01 09:23:23 +0100

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+        <v-navigation-drawer v-model="sideNav">
+            <v-list>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>supervisor_account</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>login</v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark class="primary">
+            <v-toolbar-side-icon
+                    @click.stop="sideNav = !sideNav"
+                    class="hidden-xs-and-up "></v-toolbar-side-icon>
+            <v-toolbar-title>Checkers</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn flat>
+                    <v-icon left dark>supervisor_account</v-icon>
+                    login
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <main>
+
+        </main>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                sideNav: false,
+
+            }
+        }
+    }
+</script>
+
+<style lang="stylus">
+    @import '../node_modules/vuetify/src/stylus/main.styl'
+
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,15 @@
+import App from './App'
+import router from './router'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import('../node_modules/vuetify/dist/vuetify.min.css')
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
